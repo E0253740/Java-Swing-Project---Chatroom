320f5b174e928712ba42010dfd596ae7b497e8b6
XING DA <66471809+E0253740@users.noreply.github.com>
2021-07-20 15:03:37 +0800

    Add files via upload

--- BeatBoxFinal.java
+++ BeatBoxFinal.java
@@ -1,5 +1,7 @@
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.sound.midi.*;
 import java.util.*;
 import java.awt.event.*;
@@ -10,16 +12,16 @@ import java.net.Socket;
 public class BeatBoxFinal {
     JFrame theFrame;
     JPanel mainPanel;
-    JList incomingList;
+    JList<String> incomingList;
     JTextField userMessage;
-    ArrayList<JCheckBox> checkboxList;  // checkbox存储在ArrayList中
+    ArrayList<JCheckBox> checkboxList;  // checkbox is stored in ArrayList
     int nextNum;
-    Vector<String> listVector = new Vector<>();
+    Vector<String> listVector = new Vector<String>();
     String userName;
     ObjectOutputStream out;
     ObjectInputStream in;
     HashMap<String,boolean[]> otherSeqsMap = new HashMap<>();
-
+    
     Sequencer sequencer;
     Sequence sequence;
     Sequence mySequence = null;
@@ -32,13 +34,14 @@ public class BeatBoxFinal {
     int[] instruments = {35,42,46,38,49,39,50,60,70,72,64,56,58,47,67,63};
     public static void main(String[] args) {
         new BeatBoxFinal().startUp(args[0]); // e.g. %java BeatBoxFinal theFlash
+        // args[0] is the screen name or username
     }
 
     public void startUp (String name){
         userName = name;
         // open connection to the server
         try {
-            Socket sock = new Socket("localhost",5000);
+            Socket sock = new Socket("localhost",5000); // Here I just use the local host, choose port 5000
             out = new ObjectOutputStream(sock.getOutputStream());
             in = new ObjectInputStream(sock.getInputStream());
             Thread remote = new Thread(new RemoteReader());
@@ -56,7 +59,7 @@ public class BeatBoxFinal {
         JPanel background = new JPanel(layout);
         background.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
-        // 创建右侧Button Box
+        // create Button Box on the right
         checkboxList = new ArrayList<JCheckBox>();
         Box buttonBox = new Box(BoxLayout.Y_AXIS);
 
@@ -84,7 +87,7 @@ public class BeatBoxFinal {
         buttonBox.add(userMessage);
 
         // The components which can show the message received
-        incomingList = new JList();
+        incomingList = new JList<String>();
         incomingList.addListSelectionListener(new MyListSelectionListener());
         incomingList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         JScrollPane theList = new JScrollPane(incomingList);
@@ -92,7 +95,7 @@ public class BeatBoxFinal {
         incomingList.setListData(listVector);
 
 
-        // 创建左侧Name Box
+        // create Name Box on the left
         Box nameBox = new Box(BoxLayout.Y_AXIS);
         for(int i = 0; i<16; i++){
             nameBox.add(new Label(instrumentNames[i]));
@@ -103,7 +106,7 @@ public class BeatBoxFinal {
 
         theFrame.getContentPane().add(background);
 
-        // 创建中间 mainPanel, 并且添加16*16 grid
+        // create mainPanel in the middle, and add 16*16 grid
         GridLayout grid = new GridLayout(16,16);
         grid.setVgap(1);
         grid.setHgap(2);
@@ -136,7 +139,7 @@ public class BeatBoxFinal {
         catch(Exception e) {e.printStackTrace();}
     }
 
-    // Important: 将复选框状态转换为MIDI事件并加到track上
+    // Important: transfer the selection to MIDI and add onto the track
     public void buildTrackAndStart(){
         ArrayList<Integer> tracklist = null; // This will hold the instrument for each
         sequence.deleteTrack(track);
@@ -150,11 +153,11 @@ public class BeatBoxFinal {
                 if(jc.isSelected()) {
                     int key = instruments[i];
                     tracklist.add(key);
-                } // 如果有勾选，将关键字值放在数组的该位置上，不然的话就补0
+                } 
+                // If it's selected put the key at the position, or add 0
                 else tracklist.add(null);
             }
-            makeTracks(tracklist); // 创建此乐器的事件并加到track上
-            track.add(makeEvent(192,9,1,0,15));
+            makeTracks(tracklist); // add to track
         }
         track.add(makeEvent(192,9,1,0,15));
         try{
@@ -165,7 +168,7 @@ public class BeatBoxFinal {
         }catch (Exception e){e.printStackTrace();}
     }
 
-    public class MyStartListener implements ActionListener{ // 第一个内部类，是按钮的监听者
+    public class MyStartListener implements ActionListener{ // this is the first inne class, is listener of the button
         public void actionPerformed(ActionEvent a){
             buildTrackAndStart();
         }
@@ -180,7 +183,7 @@ public class BeatBoxFinal {
     public class MyUpTempoListener implements ActionListener {
         public void actionPerformed(ActionEvent a){
             float tempoFactor = sequencer.getTempoFactor();
-            sequencer.setTempoFactor((float)(tempoFactor *1.03)); // 节奏引子，预设为1.0 每次调整3%
+            sequencer.setTempoFactor((float)(tempoFactor *1.03)); // first set as 1.0, then adjust 3% every time
         }
     }
 
@@ -191,19 +194,83 @@ public class BeatBoxFinal {
         }
     }
 
-    public class MySendListener implements ActionListner {
+    public class MySendListener implements ActionListener {
         public void actionPerformed (ActionEvent a) {
-            
+            // make an arraylist of just the STATE of the checkboxes
+            boolean[] checkboxState = new boolean[256];
+            for(int i = 0; i < 256; i++) {
+                JCheckBox check = (JCheckBox) checkboxList.get(i);
+                if(check.isSelected()) checkboxState[i] = true;
+            }
+            String messageToSend = null;
+            try {
+                out.writeObject(userName + nextNum + ": " + userMessage.getText());
+                out.writeObject(checkboxState);
+            } catch (Exception ex) {
+                System.out.println("Sorry dude. Could not send it to the server");
+            }
+            userMessage.setText("");
         }
     }
 
-    public void makeTracks (int[] list){
-        for(int i=0;i<16;i++){
-            int key = list[i];
+    public class MyListSelectionListener implements ListSelectionListener {
+        public void valueChanged (ListSelectionEvent le) {
+            if (!le.getValueIsAdjusting()) {
+                String selected = (String) incomingList.getSelectedValue();
+                if(selected!=null) {
+                    boolean[] selectedState = (boolean[]) otherSeqsMap.get(selected);
+                    changeSequence(selectedState);
+                    sequencer.stop();
+                    buildTrackAndStart();
+                } // When the user clicks information, will load sequence and start playing
+            }
+        }
+    }
 
-            if (key != 0) {
-                track.add(makeEvent(144,9,key, 100, i));
-                track.add(makeEvent(128,9,key, 100, i+1));
+    public class RemoteReader implements Runnable {
+        boolean[] checkboxState = null;
+        String nameToShow = null;
+        Object obj = null;
+        public void run() {
+            try {
+                while((obj=in.readObject()) != null) {
+                    System.out.println("got an object from server");
+                    System.out.println(obj.getClass());
+                    String nameToShow = (String) obj;
+                    checkboxState = (boolean[]) in.readObject();
+                    otherSeqsMap.put(nameToShow, checkboxState);
+                    listVector.add(nameToShow);
+                    incomingList.setListData(listVector);
+                }
+            } catch (Exception ex) {ex.printStackTrace();}
+        }
+    }
+
+    public class MyPlayMineListener implements ActionListener {
+        public void actionPerformed (ActionEvent a) {
+            if (mySequence != null) {
+                sequence = mySequence;
+            }
+        }
+    }
+
+    public void changeSequence (boolean[] checkboxState) {
+        for(int i = 0; i < 256; i++) {
+            JCheckBox check = (JCheckBox) checkboxList.get(i);
+            if(checkboxState[i]) {
+                check.setSelected(true);
+            } else {check.setSelected(false);}
+        }
+    }
+
+    public void makeTracks (ArrayList<Integer> list){
+        Iterator<Integer> it = list.iterator();
+        for(int i=0;i<16;i++){
+            Integer num = (Integer) it.next();
+            if (num != null) {
+                int numKey = num.intValue();
+                track.add(makeEvent(144,9,numKey, 100, i));
+                track.add(makeEvent(128,9,numKey, 100, i+1));
             }
         }
     }
